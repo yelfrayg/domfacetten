587c4251f083922312d1928ddac28c69a62f45cb
Yassin El Fraygui <yassin.el_fraygui@smail.th-koeln.de>
2026-07-19 20:55:14 +0200

    Fix: change Panel

--- frontend/scripts/changeProduct.js
+++ frontend/scripts/changeProduct.js
@@ -55,22 +55,20 @@ async function loadAllProducts() {
         const req = await fetch("http://localhost:3000/api/products");
         const res = await req.json();
         if (res) {
-            let a = 1;
-            res.products.forEach((p) => {
+            res.products.forEach((p, index) => {
                 const createListElement = document.createElement("li");
                 createListElement.classList.add("list-element");
                 createListElement.innerHTML = `
-                    <p>${a}</p>
+                    <p>${index + 1}</p>
                     <p>${p.arttype}${String(p.artnr).padStart(3, '0')}</p>
                     <p>${p.name}</p>
-                    <p class="price">${p.price}0 &euro;</p>
+                    <p class="price">${parseFloat(p.price).toFixed(2).replace('.', ',')} &euro;</p>
                     <div>
-                        <button type="button" class="editBtn" data-arttype="${p.arttype}" data-artnr="${p.artnr}" popovertarget="popup-edit"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></button>
-                        <button type="button" class="deleteBtn" data-arttype="${p.arttype}" data-artnr="${p.artnr}" popovertarget="popup-delete"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+                        <button type="button" class="editBtn" data-arttype="${p.arttype}" data-artnr="${p.artnr}" popovertarget="popup-edit"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg></button>
+                        <button type="button" class="deleteBtn" data-arttype="${p.arttype}" data-artnr="${p.artnr}" popovertarget="popup-delete"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
                     </div>
                 `;
                 container.append(createListElement);
-                a++;
             });
         }
     } catch (error) {
